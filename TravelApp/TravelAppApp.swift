66a05062be90a7a87d33b9eb325554776c16b4ce
huynguyen2842 <gianghuy2804@gmail.com>
2023-08-04 13:11:05 +0700

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Nguyen Giang Huy on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
