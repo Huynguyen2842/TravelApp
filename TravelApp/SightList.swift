2bfbaa3616e86ef724900be2ac907fcd46f4a24b
huynguyen2842 <gianghuy2804@gmail.com>
2023-08-05 16:19:13 +0700

    Add dark mode light mode into the app

--- TravelApp/SightList.swift
+++ TravelApp/SightList.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct SightList: View {
+    @AppStorage("isDrakMode") private var isDark = false
     @EnvironmentObject var modelData: ModelData
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
+    @State private var searchTerm = ""
     var filteredSights: [Sight] {
         modelData.sights.filter { sight in
-               (!showFavoritesOnly || sight.isFavorited)
+            (!showFavoritesOnly || sight.isFavorited) &&
+                        (searchTerm.isEmpty || sight.name.lowercased().contains(searchTerm.lowercased()))
            }
-       }
+    }
     var body: some View {
         NavigationView {
             List{
@@ -29,8 +32,17 @@ struct SightList: View {
                     }
                 }
             }
+            .searchable(text: $searchTerm, prompt: "Search Sights")
             .navigationTitle("Sights")
+            .toolbar{
+                ToolbarItem(placement: ToolbarItemPlacement.navigationBarTrailing){
+                    Button(action: {isDark.toggle()}, label: {isDark ? Label("Dark", systemImage: "sun.max.fill")
+                        : Label("Dark", systemImage: "moon.fill")
+                    })
+                }
+            }
         }
+        .environment(\.colorScheme, isDark ? .dark: .light)
     }
     
     struct SightList_Previews: PreviewProvider {
